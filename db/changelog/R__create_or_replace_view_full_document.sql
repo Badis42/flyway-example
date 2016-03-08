1c7fd1513e94b1dcafc1e9fa716275f822a90a96
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-03-08 19:52:24 +0300

    Change repeatable migration example with newest view definition.

--- db/changelog/R__create_or_replace_view_full_document.sql
+++ db/changelog/R__create_or_replace_view_full_document.sql
@@ -6,5 +6,9 @@ create view full_document as
   select
     d.id    document_id,
     d.name  document_name,
-    d.value document_value
-  from document d;
+    d.value document_value,
+    p.id    person_id,
+    p.name  person_name
+  from document d
+    join person p
+      on d.person_id = p.id;
